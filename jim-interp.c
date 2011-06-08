3f857a9b3ff741307c2bdba0aac50fdc34617fb8
Steve Bennett <steveb@workware.net.au>
2011-06-08 10:00:54 +1000

    Add basic support for sub-interpreters
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

---
+++ jim-interp.c
@@ -0,0 +1,121 @@
+#include "jim.h"
+#include "jimautoconf.h"
+#include "jim-subcmd.h"
+
+struct InterpInfo
+{
+    Jim_Interp *interp;
+};
+
+static void JimInterpDelProc(Jim_Interp *interp, void *privData)
+{
+    struct InterpInfo *iis = privData;
+
+    Jim_FreeInterp(iis->interp);
+    Jim_Free(iis);
+}
+
+static int interp_cmd_eval(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
+{
+    int ret;
+    Jim_Interp *subinterp = ((struct InterpInfo *)Jim_CmdPrivData(interp))->interp;
+
+    /* Evaluate the command in the given interpreter.
+     * Everything passing between the two interpreters must be converted to
+     * a string first.
+     */
+    Jim_Obj *scriptStringObj = Jim_ConcatObj(interp, argc, argv);
+    Jim_Obj *scriptObj = Jim_NewStringObj(subinterp, Jim_String(scriptStringObj), -1);
+    Jim_FreeNewObj(interp, scriptStringObj);
+    Jim_IncrRefCount(scriptObj);
+    ret = Jim_EvalObj(subinterp, scriptObj);
+    Jim_DecrRefCount(subinterp, scriptObj);
+
+    /* And extract the result */
+    Jim_SetResultString(interp, Jim_String(Jim_GetResult(subinterp)), -1);
+
+    return ret;
+}
+
+static int interp_cmd_delete(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
+{
+    return Jim_RenameCommand(interp, Jim_String(argv[0]), "");
+}
+
+static const jim_subcmd_type interp_command_table[] = {
+    {   .cmd = "eval",
+        .args = "script ...",
+        .function = interp_cmd_eval,
+        .minargs = 1,
+        .maxargs = -1,
+        .description = "Concat the args and evaluate the script in the interpreter"
+    },
+    {   .cmd = "delete",
+        .function = interp_cmd_delete,
+        .flags = JIM_MODFLAG_FULLARGV,
+        .description = "Delete this interpreter"
+    },
+    { 0 }
+};
+
+static int JimInterpSubCmdProc(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
+{
+    return Jim_CallSubCmd(interp, Jim_ParseSubCmd(interp, interp_command_table, argc, argv), argc, argv);
+}
+
+static void JimInterpCopyVariable(Jim_Interp *target, Jim_Interp *source, const char *var, const char *default_value)
+{
+    Jim_Obj *value = Jim_GetGlobalVariableStr(source, var, JIM_NONE);
+    const char *str;
+
+    if (value) {
+        str = Jim_String(value);
+    }
+    else {
+        str = default_value;
+    }
+    if (str) {
+        Jim_SetGlobalVariableStr(target, var, Jim_NewStringObj(target, str, -1));
+    }
+}
+
+/**
+ * [interp] creates a new interpreter.
+ */
+static int JimInterpCommand(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
+{
+    struct InterpInfo *iis;
+    char buf[32];
+
+    if (argc != 1) {
+        Jim_WrongNumArgs(interp, 1, argv, "");
+        return JIM_ERR;
+    }
+
+    /* Create the interpreter command */
+    iis = Jim_Alloc(sizeof(*iis));
+    iis->interp = Jim_CreateInterp();
+    Jim_RegisterCoreCommands(iis->interp);
+    Jim_InitStaticExtensions(iis->interp);
+
+    /* Copy some core variables to the new interpreter */
+    JimInterpCopyVariable(iis->interp, interp, "argv", "");
+    JimInterpCopyVariable(iis->interp, interp, "argc", "0");
+    JimInterpCopyVariable(iis->interp, interp, "argv0", NULL);
+    JimInterpCopyVariable(iis->interp, interp, "jim_argv0", NULL);
+
+    snprintf(buf, sizeof(buf), "interp.handle%ld", Jim_GetId(interp));
+    Jim_CreateCommand(interp, buf, JimInterpSubCmdProc, iis, JimInterpDelProc);
+    Jim_SetResultString(interp, buf, -1);
+    return JIM_OK;
+}
+
+int Jim_interpInit(Jim_Interp *interp)
+{
+    if (Jim_PackageProvide(interp, "interp", "1.0", JIM_ERRMSG))
+        return JIM_ERR;
+
+    Jim_CreateCommand(interp, "interp", JimInterpCommand, NULL, NULL);
+
+    return JIM_OK;
+}
